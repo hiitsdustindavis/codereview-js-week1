e81564a84f2856f1fd42b3640ff05ba9bfc011b1
Josh and Nicholas Jensen-Hay <student@epicodus.com>
2016-03-15 09:59:32 -0700

    update to include node/bower/gulp/yada/yada/yada

---
+++ createproject.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+# read input of project name from user
+# make project folder
+# make js, spec and css folders in project folder
+# copy in jquery, mocha, chair, and spec files in
+# intialize git in the folder
+# ask to run pairs command to set authors
+# create initial commit with blank files
+./paircreator.sh
+clear
+echo What is the project called?
+read project
+
+if [ ! -d "$project" ]; then # if the directory does not exist
+  mkdir ../"$project"
+  mkdir ../"$project"/js ../"$project"/scss
+  cp cp/js/* ../"$project"/js/
+  cp cp/scss/* ../"$project"/scss/
+  cp cp/* ../"$project"/
+  echo "Directory created and files added"
+else
+  echo "Directory already exists"
+fi
+
+
+# make sure that works then
+
+GIT_ADD="."
+INITIAL_COMMIT_MESSAGE="Initial Commit"
+
+cd ../"$project"
+
+echo "Enter pairs intials"
+read pairinitial
+
+
+echo "Opening directory in atom"
+atom .
+
+npm install
+bower install
+
+if [ -d ".git" ]; then
+  echo "Git already init"
+else
+  git init
+  if (( $? )); then
+    echo "Unable to git init"
+  fi
+  git pair njh "$pairinitial"
+  if (( $? )); then
+    echo "Unable to set authors"
+  fi
+  git add "$GIT_ADD"
+  if (( $? )); then
+    echo "Unable to git add"
+  fi
+  git commit -m "$INITIAL_COMMIT_MESSAGE"
+  if (( $? )); then
+    echo "Unable to git commit"
+  fi
+  echo "The directory was initialized"
+fi
+
+gulp build
+gulp serve
---
+++ paircreator.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "Pair file creation!"
+function writeFile {
+FILE=~/.pairs
+
+# Was setup to enter both pairs name and intials
+# echo "  $initial1: $name1" >> "$FILE"
+# echo "  $initial1: $email1" >> "$FILE"
+
+# now set to mine(Nicholas Jensen-Hay)
+echo pairs: > ""$FILE""
+echo "  njh: Nicholas Jensen-Hay" >> "$FILE"
+echo "  $initial2: $name2" >> "$FILE"
+echo email_addresses: >> "$FILE"
+echo "  njh: nicholasjensenhay@gmail.com" >> "$FILE"
+echo "  $initial2: $email2" >> "$FILE"
+
+echo "$FILE" file written.
+}
+
+function collectInput {
+clear
+# echo First pairs initials, followed by enter
+# read initial1
+# clear
+# echo First pairs name, followed by enter
+# read name1
+# clear
+# echo First pairs email, followed by enter
+# read email1
+# clear
+echo Enter pairs initials, followed by enter
+read initial2
+clear
+echo Enter pairs name, followed by enter
+read name2
+clear
+echo Enter pairs email, followed by enter
+read email2
+clear
+}
+
+FILE=~/.pairs
+if [ -f "$FILE" ]
+then
+  echo File exists. Overwrite? y/n [case sensitive]
+  read answer
+  if [[ "$answer" == y* ]]
+    then collectInput
+    writeFile
+  else echo Aborted
+  fi
+else collectInput
+  writeFile
+fi
