b8f7b26e096b6fb56f0c24ee1b81d64f2f7052ca
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-17 08:29:15 -0700

    update serve.sh for the if to work properly

--- cp/serve.sh
+++ cp/serve.sh
@@ -1,12 +1,12 @@
-if[ ! -d "node_modules" ]
+if [ ! -d "./node_modules" ]; then
   npm install
 fi
 
-if[ ! -d "bower_componenets" ]
+if [ ! -d "bower_componenets" ]; then
   bower install
 fi
 
-if[ ! -d "build" ]
+if [ ! -d "build" ]; then
   gulp build
 fi
 
